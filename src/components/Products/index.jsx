f0a4c5e55747d35a1973e7faf69980ff1242ed0c
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-08-06 19:17:40 -0300

    Feat: Loading component finish

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -8,19 +8,21 @@ import Loading from "../Loading";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
     fetchProducts('iphone').then((response)=>{
       setProducts(response);
+      setLoading(false)
     })
   },[])
 
-
   return ( 
-    <section className="products container">
-      <Loading />
-      {products.map((product)=> <ProductCard key={product.id} data={product}/>)}
-    </section>
+    (loading && <Loading />) || (
+      <section className="products container">  
+        {products.map((product)=> <ProductCard key={product.id} data={product}/>)}
+      </section>
+    )     
    );
 }
 

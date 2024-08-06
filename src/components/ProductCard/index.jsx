d8a253383f7f6ea1604fa37f9d1645c4a72cda35
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-08-06 16:17:08 -0300

    Feat: add list of products

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,19 +1,21 @@
 import { BsFillCartPlusFill } from "react-icons/bs";
 
+import propTypes from 'prop-types'
 import React from "react";
 import './styles.css'
 
-function ProductCard() {
+function ProductCard({data}) {
+  const {title, thumbnail, price} = data;
   return ( 
     <section className="product-card">
       <img 
-        src="https://http2.mlstatic.com/D_918579-MLM51559384401_092022-W.jpg" 
+        src={thumbnail} 
         alt="product" 
         className="card__image"/>
 
       <div className="card__infos">
-        <h2 className="card__price">R$ 200.20</h2>
-        <h2 className="card__title">Produto</h2>
+        <h2 className="card__price">{price}</h2>
+        <h2 className="card__title">{title}</h2>
       </div>
       <button type="button" className="button__add-cart">
         <BsFillCartPlusFill size={30}/>
@@ -23,3 +25,7 @@ function ProductCard() {
 }
 
 export default ProductCard;
+
+ProductCard.prototype = {
+  data: propTypes.shape({})
+}.isRequired;

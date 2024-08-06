72f9ca4f25c835a488faf7aafba080e22e481f2b
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-08-06 11:27:43 -0300

    Feat: add state from search

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import { CiSearch } from "react-icons/ci";
 
 import './styles.css'
 
 function SearchBar() {
+const [searchValue, setSearchValue] = useState('')
+
   return ( 
   <form className='search-bar'>
     <input 
-      type="search" 
+      type="search"
+      value={searchValue}
       placeholder='Buscar produtos' 
       className='search__input' 
+      onChange={({target}) => setSearchValue(target.value)}
       required
     />
     <button type='button' className='search__button'>

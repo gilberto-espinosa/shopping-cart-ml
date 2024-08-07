9815e9f5cc39e4af2b2b05ad468a9f5139775df4
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-08-06 21:53:47 -0300

    Feat: add logic in CartButton

--- src/components/CartButton/index.jsx
+++ src/components/CartButton/index.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BsCart } from "react-icons/bs";
 
 import './styles.css'
+import AppContext from '../../context/AppContext';
 
 function CartButton() {
+  const { cartItems } = useContext(AppContext)
+
   return ( 
   <button className="cart__button">
     <BsCart />
-    <span className="cart__status">1</span>
+    {cartItems.length> 0 && (
+      <span className="cart__status">{cartItems.length}</span>
+    )}
   </button> 
   );
 }
